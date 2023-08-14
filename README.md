05d9325011b1779ede569f0666092bc01eb2b72a
Asazu Taiga <taiga.asazu@icloud.com>
2023-08-15 01:23:03 +0900

    Initial commit - code-guide

---
+++ README.md
@@ -0,0 +1,33 @@
+# Code Guide
+
+<h1 align="center">
+ <img src="https://github.com/AsazuTaiga/Code-Guide/blob/main/logo.png?raw=true" alt="Code Guide">
+</h1>
+
+Code Guide is a Visual Studio Code extension that uses OpenAI's API key to provide a description of the currently viewed file.
+
+## Features
+
+- Provides a brief description of the currently viewed file using OpenAI's API key.
+- Allows users to select the language of the description.
+- Can be accessed through the command palette.
+
+## Installation
+
+To install Code Guide, follow these steps:
+
+1. Open Visual Studio Code.
+2. Click on the Extensions icon in the left-hand sidebar.
+3. Search for "Code Guide" in the search bar.
+4. Click on the "Install" button next to the Code Guide extension.
+5. Wait for the installation to complete.
+
+## Usage
+
+- `Code Guide: Describe Code in This File`: Provides a description of the currently viewed file.
+- `Code Guide: Set OpenAI Key`: Allows users to set their OpenAI API key.
+- `Code Guide: Set Language`: Allows users to set the language of the description.
+
+## License
+
+Code Guide is licensed under the MIT License. See the LICENSE file for more information.
---
+++ src/extension.ts
@@ -0,0 +1,159 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+import { Configuration, OpenAIApi } from "openai";
+import { marked } from "marked";
+import { langs } from "./languages";
+
+const setApiKey = vscode.commands.registerCommand(
+  "code-guide.setOpenAiKey",
+  () => {
+    vscode.window
+      .showInputBox({
+        placeHolder: "OpenAI API Key",
+        prompt: "Enter your OpenAI API key",
+      })
+      .then((apiKey) => {
+        if (!apiKey) {
+          return;
+        }
+        vscode.workspace
+          .getConfiguration("code-guide")
+          .update("apiKey", apiKey, true);
+        // success
+        vscode.window.showInformationMessage("Code Guide: OpenAI API key set.");
+      });
+  }
+);
+
+const setLanguage = vscode.commands.registerCommand(
+  "code-guide.setLanguage",
+  () => {
+    // select language
+    vscode.window
+      .showQuickPick(langs.map((lang) => ({ label: lang })))
+      .then((language) => {
+        if (!language) {
+          return;
+        }
+        vscode.workspace
+          .getConfiguration("code-guide")
+          .update("language", language.label, true);
+        // success
+        vscode.window.showInformationMessage(
+          `Code Guide: Language set to ${language.label}.`
+        );
+      });
+  }
+);
+
+const configuration = new Configuration({
+  apiKey: vscode.workspace.getConfiguration("code-guide").get("apiKey"),
+});
+const openai = new OpenAIApi(configuration);
+
+const generateFileDescriptionPrompt = (text: string, language: string) => {
+  return `\
+You are a programmer helping a new programmer understand the code in this file.
+Please describe the code in ${language}.
+
+Output Format (Markdown):
+
+# Overview, Framework, Library, or API
+# Context, Background, and Motivation
+# Where to start reading
+# Which files to read next
+# Prerequisites
+
+Input Code:
+
+${text}
+`;
+};
+
+// describe code
+const describeCode = vscode.commands.registerCommand(
+  "code-guide.describeCodeInThisFile",
+  () => {
+    vscode.window.withProgress(
+      {
+        location: vscode.ProgressLocation.Notification,
+        title: "Code Guide: Generating description...",
+        cancellable: false,
+      },
+      async () => {
+        // check api key
+        const apiKey = vscode.workspace
+          .getConfiguration("code-guide")
+          .get("apiKey");
+        if (!apiKey) {
+          vscode.window.showErrorMessage(
+            "Code Guide: Please set your OpenAI API key in the Code Guide extension settings."
+          );
+          return;
+        }
+
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+          return;
+        }
+        // get full content of the file
+        const document = editor.document;
+        const text = document.getText();
+        // get the language from config (English, Chinese, etc.)
+        let language = vscode.workspace
+          .getConfiguration("code-guide")
+          .get("language");
+        if (typeof language !== "string") {
+          language = "English";
+        }
+        // create chat completion
+        try {
+          const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+              {
+                role: "user",
+                content: generateFileDescriptionPrompt(
+                  text,
+                  language as string
+                ),
+              },
+            ],
+          });
+          const responseText = response.data.choices[0]?.message?.content;
+          if (!responseText) {
+            vscode.window.showErrorMessage(
+              "Code Guide: Failed to generate description."
+            );
+            return;
+          }
+          // render markdown in new tab (beside the code)
+          const panel = vscode.window.createWebviewPanel(
+            "codeGuide",
+            "Code Guide",
+            vscode.ViewColumn.Beside,
+            {}
+          );
+          panel.webview.html = marked(responseText);
+        } catch (error) {
+          vscode.window.showErrorMessage(
+            "Code Guide: Failed to generate description."
+          );
+          return;
+        }
+      }
+    );
+  }
+);
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(setApiKey);
+  context.subscriptions.push(setLanguage);
+  context.subscriptions.push(describeCode);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
